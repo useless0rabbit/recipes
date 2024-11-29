63150fc612ff1559a6979534a5f888754569b242
ILYA BUROV <iburov@korusconsulting.ru>
2024-11-29 09:16:35 +0500

    final1

--- src/components/DifficultyButton.jsx
+++ src/components/DifficultyButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import cn from 'classnames';
-import './DifficultyButton.css';
+import './DifficultyButton.scss';
 
 const DifficultyButton = ({ label, isActive, onClick }) => {
   return (
